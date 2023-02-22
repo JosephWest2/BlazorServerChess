64dc248633ac967d18c5cdafad98134a01014ab5
JWest11 <114183436+JWest11@users.noreply.github.com>
2023-02-22 14:50:21 -0700

    Update 10

---
+++ Data/ChessGame/ServerGame.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorServerChess.Data.ChessGame
+{
+	public class ServerGame
+	{
+		public string PlayerOneId { get; }
+		public ColorEnum PlayerOneColor { get; }
+		public string PlayerTwoId { get; }
+		public ColorEnum PlayerTwoColor { get; }
+
+		public ServerGame(string playerOneId, string playerTwoId)
+		{
+			PlayerOneId = playerOneId;
+			PlayerTwoId = playerTwoId;
+			var rand = new Random();
+			if (rand.NextDouble() < 0.5)
+			{
+				PlayerOneColor = ColorEnum.White;
+				PlayerTwoColor = ColorEnum.Black;
+			}
+			else
+			{
+				PlayerOneColor = ColorEnum.Black;
+				PlayerTwoColor = ColorEnum.White;
+			}
+		}
+	}
+}
